455575d9fbc97a3a779966036d120151e55ae3d4
Sebastianleon_h <Sebastianleon_h@LAB4-5>
2024-02-22 09:40:50 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace ListasConObjetos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxApellido = new System.Windows.Forms.TextBox();
+            this.textBoxDPI = new System.Windows.Forms.TextBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnOrderDescend = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "DPI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fecha de nacimiento";
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(167, 32);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(248, 20);
+            this.textBoxNombre.TabIndex = 4;
+            // 
+            // textBoxApellido
+            // 
+            this.textBoxApellido.Location = new System.Drawing.Point(167, 80);
+            this.textBoxApellido.Name = "textBoxApellido";
+            this.textBoxApellido.Size = new System.Drawing.Size(248, 20);
+            this.textBoxApellido.TabIndex = 5;
+            // 
+            // textBoxDPI
+            // 
+            this.textBoxDPI.Location = new System.Drawing.Point(167, 127);
+            this.textBoxDPI.Name = "textBoxDPI";
+            this.textBoxDPI.Size = new System.Drawing.Size(248, 20);
+            this.textBoxDPI.TabIndex = 6;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(167, 169);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 7;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(340, 361);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 8;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(461, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(471, 296);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(857, 345);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 10;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(938, 35);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(75, 23);
+            this.btnOrdenar.TabIndex = 11;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(259, 361);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 12;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnOrderDescend
+            // 
+            this.btnOrderDescend.Location = new System.Drawing.Point(938, 77);
+            this.btnOrderDescend.Name = "btnOrderDescend";
+            this.btnOrderDescend.Size = new System.Drawing.Size(75, 23);
+            this.btnOrderDescend.TabIndex = 13;
+            this.btnOrderDescend.Text = "Ordenar desc";
+            this.btnOrderDescend.UseVisualStyleBackColor = true;
+            this.btnOrderDescend.Click += new System.EventHandler(this.btnOrderDescend_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(938, 120);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 14;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(938, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Leer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnOrderDescend);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.textBoxDPI);
+            this.Controls.Add(this.textBoxApellido);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxApellido;
+        private System.Windows.Forms.TextBox textBoxDPI;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnOrderDescend;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListasConObjetos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+
+        }
+
+        List<Persona> personas = new List<Persona>();
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+
+            //instancia la clase
+            Persona Persona = new Persona();
+
+            Persona.Nombre = textBoxNombre.Text;
+            Persona.Apellido = textBoxApellido.Text;
+            Persona.Dpi = textBoxDPI.Text;
+            Persona.FechaNacimiento = monthCalendar1.SelectionStart;
+
+            //se guarda la persona en la lista de personas
+            personas.Add(Persona);
+
+            textBoxNombre.Text = ("");
+            textBoxApellido.Text = ("");
+            textBoxDPI.Text = ("");
+
+        }
+
+        private void Mostrar()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = personas;
+            dataGridView1.Refresh();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            Mostrar();
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            personas = personas.OrderBy(p => p.Apellido).ToList();
+            Mostrar();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            string dpi = textBoxDPI.Text;
+
+            personas.RemoveAll(p => p.Dpi == dpi);
+
+            Mostrar();
+        }
+
+        private void btnOrderDescend_Click(object sender, EventArgs e)
+        {
+            personas.OrderByDescending(p => p.Apellido);
+            Mostrar();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            
+            FileStream stream = new FileStream("personas.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            
+            StreamWriter writer = new StreamWriter(stream);
+
+            for (int i = 0; i < personas.Count; i++)
+            {
+                writer.WriteLine(personas[i].Nombre);
+                writer.WriteLine(personas[i].Apellido);
+                writer.WriteLine(personas[i].Dpi);
+                writer.WriteLine(personas[i].FechaNacimiento.ToShortDateString());
+            }
+
+            MessageBox.Show("Guardado");
+            
+            
+            writer.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string fileName = "personas.txt";
+
+           
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+           
+            while (reader.Peek() > -1)
+            
+            {
+                Persona persona = new Persona();
+
+                persona.Nombre = reader.ReadLine();
+                persona.Apellido = reader.ReadLine();
+                persona.Dpi = reader.ReadLine();
+                persona.FechaNacimiento = Convert.ToDateTime(reader.ReadLine());
+
+                personas.Add(persona);
+            }
+            
+            reader.Close();
+            Mostrar();
+        }
+    }
+}
